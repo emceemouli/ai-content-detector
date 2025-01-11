6b53bdcca6698079c2050b995edbeb8a5a1dc959
Mouli Chandramouli <mchandramouli@gmail.com>
2025-01-11 13:00:48 -0700

    Adsense

--- components/Adsense.tsx
+++ components/Adsense.tsx
@@ -1,47 +1,23 @@
-import { useEffect, useState } from 'react';
-
-declare global {
-  interface Window {
-    adsbygoogle: {
-      push: (config?: unknown) => void;
-    }[];
-  }
-}
+import { useEffect } from 'react';
 
 export const InArticleAd: React.FC = () => {
-  const [adsbygoogle, setAdsbygoogle] = useState<Window['adsbygoogle'] | null>(null);
-
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const script = document.createElement('script');
       script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7638771792216412';
       script.async = true;
-      script.onload = () => {
-        setAdsbygoogle(window.adsbygoogle || []);
-
-        if (window.adsbygoogle) {
-          try {
-            // Use a type assertion to tell TypeScript to treat {} as the correct type
-            window.adsbygoogle.push({} as unknown); 
-          } catch (err) {
-            console.error('AdSense error:', err);
-          }
-        }
-      };
       document.head.appendChild(script);
     }
   }, []);
 
   return (
     <div className="my-8">
-      {adsbygoogle && (
-        <ins className="adsbygoogle"
-          style={{ display: 'block', textAlign: 'center' }}
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="pub-7638771792216412">
-        </ins>
-      )}
+      <ins className="adsbygoogle"
+        style={{ display: 'block', textAlign: 'center' }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="pub-7638771792216412">
+      </ins>
     </div>
   );
 };
